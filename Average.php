2ae5dfa2652d0539f064d84be98eb38c46a9036e
Sulthan Saalim Rabbani Atmadja <126134793+SulthanSaalimRabbaniAtmadja@users.noreply.github.com>
2023-04-07 22:24:57 +0700

    Update Average.php

--- Average.php
+++ Average.php
@@ -1,12 +1,12 @@
 <?php
 
-$nilai_matematika = 60;
-$nilai_fisika = 80;
+$nilai_matematika = 61;
+$nilai_fisika = 75;
 $nilai_biologi = 68;
 
 $ratarata = ($nilai_matematika + $nilai_fisika + $nilai_biologi) / 3;
 
-if($ratarata > 70){
+if($ratarata > 60){
     echo "$ratarata \n";
     echo "Anda Lulus";
 }else{
